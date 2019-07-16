b6c8dc3d41747edc571838e197023e2e7d114607
JacobShafer <shafer.jacob@gmail.com>
2019-07-16 10:35:01 -0500

    working for mac & linux... need to test on windows

--- pingsweep.go
+++ pingsweep.go
@@ -5,15 +5,11 @@ import (
 	"net"
 	"os"
 	"regexp"
-	"runtime"
 	"time"
 
 	"github.com/tatsushid/go-fastping"
 )
 
-/* 	to do
-make able to compile via commandline for w/e os
-*/
 func findIP(input string) string {
 	//https://www.socketloop.com/tutorials/golang-find-ip-address-from-string
 	numBlock := "(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])"
@@ -21,7 +17,6 @@ func findIP(input string) string {
 
 	regEx := regexp.MustCompile(regexPattern)
 	return regEx.FindString(input)
-	//etf
 }
 func inc(ip net.IP) {
 	for j := len(ip) - 1; j >= 0; j-- {
@@ -31,8 +26,7 @@ func inc(ip net.IP) {
 		}
 	}
 }
-func forMac() {
-	fmt.Println("OSX")
+func runIP() {
 	userIP := os.Args[1]
 	userCIDR := os.Args[2]
 	ipStr := userIP + "/" + userCIDR
@@ -64,55 +58,10 @@ func forMac() {
 
 }
 
-func forNix() {
-	fmt.Println("nix")
-	userIP := os.Args[1]
-	userCIDR := os.Args[2]
-	ipStr := userIP + "/" + userCIDR
-	ip, ipnet, err := net.ParseCIDR(ipStr)
-	if err != nil {
-		fmt.Println("ok")
-	}
-
-	p := fastping.NewPinger()
-	for ip := ip.Mask(ipnet.Mask); ipnet.Contains(ip); inc(ip) {
-		//fmt.Printf(ip.String() + "\n")
-		if len(ip.String()) > 0 {
-			p.AddIP(ip.String())
-		}
-	}
-	fmt.Println("Hosts Up:")
-	fmt.Println("================================")
-	p.OnRecv = func(addr *net.IPAddr, rtt time.Duration) {
-		fmt.Printf("IP Addr: %s receive, RTT: %v\n", addr.String(), rtt)
-	}
-	p.OnIdle = func() {
-		fmt.Println("================================")
-		fmt.Println("finish")
-	}
-	err = p.Run()
-	if err != nil {
-		fmt.Println(err)
-	}
-}
-
-/*
-func forDoze() {
-	fmt.Println("Doze")
-}
-*/
 func main() {
-	//calculate subnet
-	//specify number of pings
-	//flags for ping
-	switch os := runtime.GOOS; os {
-	case "darwin":
-		forMac()
-		/*case "linux":
-			forNix()
-		case "windows":
-			forDoze()
-		*/
-	}
+	fmt.Println("./binary 10.10.10.10 31")
+	fmt.Println("This will scan 10.10.10.10/31 and output all IP's in that range that are up")
+	fmt.Println("Due to running w/ raw sockets may have to sudo/run as root")
+	runIP()
 
 }
--- readme.md
+++ readme.md
@@ -2,7 +2,8 @@ been finding that i've needed to discover hosts on a subnet with machines that m
 
 especially cloud hosted networks with a large subnet full of hosts that change ip's daily.
 
-to do:
-- add windows functionality
-- add ability to compile for os/arch instead of at runtime
-- ... more than i can't currently think of
+
+run compile.sh to compile for windows 32/64, linux 32/64, mac 64
+can edit compile.sh to only compile for what you want
+
+due to using raw sockets, after compiling you may need to run the binaries w/ sudo/root
